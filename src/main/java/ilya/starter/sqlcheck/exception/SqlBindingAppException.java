7dbebbf586fe9e37c943904a3f1984d6e858a113
Ilya <rollovitch.roll@yandex.ru>
2024-07-27 16:15:29 +0300

    добавление исключений

---
+++ src/main/java/ilya/starter/sqlcheck/exception/SqlBindingAppException.java
@@ -0,0 +1,40 @@
+package ilya.starter.sqlcheck.exception;
+
+import ilya.starter.sqlcheck.exception.model.SqlBindProblem;
+import jakarta.annotation.Nonnull;
+
+import java.util.List;
+
+public class SqlBindingAppException extends RuntimeException{
+    private final List<SqlBindProblem> bindExceptions;
+
+    public SqlBindingAppException(@Nonnull List<SqlBindProblem> bindExceptions) {
+        super(getMessage(bindExceptions));
+        this.bindExceptions = bindExceptions;
+    }
+
+    public SqlBindingAppException(Throwable cause, @Nonnull List<SqlBindProblem> bindExceptions) {
+        super(getMessage(bindExceptions), cause);
+        this.bindExceptions = bindExceptions;
+    }
+
+    public SqlBindingAppException(String message, List<SqlBindProblem> bindExceptions) {
+        super(String.join(message, getMessage(bindExceptions)));
+        this.bindExceptions = bindExceptions;
+    }
+
+    public SqlBindingAppException(String message, Throwable cause, List<SqlBindProblem> bindExceptions) {
+        super(String.join(message, getMessage(bindExceptions)), cause);
+        this.bindExceptions = bindExceptions;
+    }
+
+    private static String getMessage(@Nonnull List<SqlBindProblem> bindExceptions) {
+        final var sb = new StringBuilder();
+        sb.append("Error with Sql binding! Reasons:\n");
+        bindExceptions.stream()
+                .map(SqlBindProblem::toMessage)
+                .forEach(sb::append);
+        return sb.toString();
+    }
+
+}
