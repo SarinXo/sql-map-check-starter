fe104bc1bac62d4ecd76aa931178f2ee68da9581
EgorovIA <rollovitch.roll@yandex.ru>
2024-08-14 12:20:17 +0300

    ref: ref config

--- src/main/java/ilya/starter/sqlcheck/configuration/SqlCheckConfig.java
+++ src/main/java/ilya/starter/sqlcheck/configuration/SqlCheckConfig.java
@@ -1,28 +1,35 @@
 package ilya.starter.sqlcheck.configuration;
 
-import ilya.starter.sqlcheck.verifyer.SimpleSqlViewLoader;
+import ilya.starter.sqlcheck.verifyer.impl.SimpleSqlViewLoader;
+import ilya.starter.sqlcheck.verifyer.impl.SimpleVerifySqlBinding;
 import ilya.starter.sqlcheck.verifyer.SqlViewLoader;
+import ilya.starter.sqlcheck.verifyer.VerifySqlBinding;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
-import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @Configuration
 @ConfigurationPropertiesScan(basePackages = "ilya.starter.sqlcheck")
-@ConditionalOnProperty(prefix = "sqlcheck", name = "enable")
+@EnableConfigurationProperties(SqlCheckProperties.class)
+@ConditionalOnProperty(prefix = "sqlcheck", name = "enable", havingValue = "true")
 public class SqlCheckConfig {
 
     @Bean
-    @Scope(SCOPE_PROTOTYPE)
     @ConditionalOnMissingBean(SqlViewLoader.class)
     public SqlViewLoader sqlViewLoader(SqlCheckProperties properties){
         return new SimpleSqlViewLoader(properties.getPathsToPackages());
     }
+
+    @Bean
+    @ConditionalOnMissingBean(VerifySqlBinding.class)
+    public VerifySqlBinding verifySqlBinding() {
+        return new SimpleVerifySqlBinding();
+    }
+
 }
 
 
